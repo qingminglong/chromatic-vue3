8934915431b1c6d5ccb2734adf0ca2dbcd0b6041
卿敏龙068001063 <qing.minlong@xydigit.com>
2025-07-20 23:15:21 +0800

    chromatic

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'cypress'
 
+import { installPlugin } from '@chromatic-com/cypress'
+
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173',
+    setupNodeEvents(on, config) {
+      installPlugin(on, config)
+    },
   },
 })
